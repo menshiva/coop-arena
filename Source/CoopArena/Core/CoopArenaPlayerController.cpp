41160b3e0b3da2ac8f9ce3cda321990d639370c7
Ivan Menshikov <menshiva@icloud.com>
2026-09-09 12:35:37 +0200

    hud

--- Source/CoopArena/Core/CoopArenaPlayerController.cpp
+++ Source/CoopArena/Core/CoopArenaPlayerController.cpp
@@ -1,14 +1,21 @@
 #include "CoopArenaPlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "Blueprint/UserWidget.h"
+
+void ACoopArenaPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	if (IsLocalPlayerController())
+		if (HudWidgetClass)
+			if (const auto HudWidget = CreateWidget<UUserWidget>(this, HudWidgetClass))
+				HudWidget->AddToViewport();
+}
 
 void ACoopArenaPlayerController::SetupInputComponent() {
 	Super::SetupInputComponent();
 
-	// only add IMCs for local player controllers
-	if (IsLocalPlayerController()) {
-		// Add Input Mapping Contexts
+	if (IsLocalPlayerController())
 		if (const auto SubsystemPtr = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
 			for (const auto CurrentContextPtr : DefaultMappingContexts)
 				SubsystemPtr->AddMappingContext(CurrentContextPtr, 0);
-	}
 }
--- Source/CoopArena/Core/CoopArenaPlayerController.h
+++ Source/CoopArena/Core/CoopArenaPlayerController.h
@@ -3,13 +3,18 @@
 #include "GameFramework/PlayerController.h"
 #include "CoopArenaPlayerController.generated.h"
 
+class UUserWidget;
 class UInputMappingContext;
 
 UCLASS(Abstract)
 class ACoopArenaPlayerController : public APlayerController {
 	GENERATED_BODY()
 protected:
+	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
+
+	UPROPERTY(EditDefaultsOnly, Category="UI")
+	TSubclassOf<UUserWidget> HudWidgetClass;
 
 	UPROPERTY(EditAnywhere, Category ="Input|Input Mappings")
 	TArray<UInputMappingContext*> DefaultMappingContexts;
